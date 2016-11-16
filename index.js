ccc8433aace66aca9dd2ee9e15d80cbc71eac34c
James Conkling <james.lane.conkling@gmail.com>
2016-11-16 15:36:38 -0500

    export temparature, population, and health datasets

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 export { default as city_temperature } from './city_temperature';
 export { default as letter_frequency } from './letter_frequency';
+export { default as new_york_temperature } from './new_york_temperature';
+export { default as state_population_by_age } from './state_population_by_age';
+export { default as wealth_and_health_of_nations } from './wealth_and_health_of_nations';
